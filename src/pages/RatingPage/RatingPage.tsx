203a0a04e5e2162cfcdcaed027a8f10d6ce432ce
B3lla <sjin09300@gmail.com>
2026-06-15 17:28:21 +0900

    feat: add RatingPage base structure

---
+++ src/pages/RatingPage/RatingPage.tsx
@@ -0,0 +1,11 @@
+function RatingPage() {
+  return (
+    <div>
+      <p>시청하셨던 영화에 별점을 매겨주세요.</p>
+      {/* 영화 목록 */}
+      <button>결과 보기</button>
+    </div>
+  );
+}
+
+export default RatingPage;
